a6ee9a71b503430e20cf7f90c99c95387fc550e9
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-03-02 20:19:52 +0000

    FIRST POST

--- zrest/src/lib.rs
+++ zrest/src/lib.rs
@@ -1,3 +1,54 @@
+pub struct Client {
+    http: reqwest::Client,
+    email: String,
+    api_key: String,
+    // TODO: Investigate keys across servers
+    server: String,
+}
+
+type HResult<T> = reqwest::Result<T>;
+
+impl Client {
+    pub fn new(email: &str, api_key: &str, server: &str) -> Self {
+        Self {
+            http: reqwest::Client::new(),
+            email: email.to_owned(),
+            api_key: api_key.to_owned(),
+            server: format!("http://{}/api/v1", server),
+        }
+    }
+
+    /// Send a message
+    ///
+    /// [Zulip docs](https://zulip.com/api/send-message)
+    pub async fn send(&self, content: &str) -> HResult<String> {
+        self.http
+            .post(&format!("{}/messages", self.server))
+            .basic_auth(&self.email, Some(&self.api_key))
+            .form(&[
+                ("type", "private"),
+                // AARON id
+                ("to", "user6@zulipdev.com"),
+                ("topic", "First Post"),
+                ("content", content),
+            ])
+            .send()
+            .await?
+            .text()
+            .await
+    }
+
+    pub async fn users(&self) -> HResult<String> {
+        self.http
+            .get(&format!("{}/users", self.server))
+            .basic_auth(&self.email, Some(&self.api_key))
+            .send()
+            .await?
+            .text()
+            .await
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
